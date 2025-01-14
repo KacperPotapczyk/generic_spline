343bd66c003eb003b92c62947a8715e39ed8a480
Kacper Potapczyk <kacper.potapczyk@gmail.com>
2025-01-14 18:03:16 +0100

    add convenience constructors and unit tests

--- src/knot.rs
+++ src/knot.rs
@@ -12,8 +12,9 @@ impl Knot {
 
         let max_derivative_degree = derivatives_values.iter()
                 .map(|kv| *kv.0)
-                .max();
-        if continuity < max_derivative_degree.unwrap_or(0) {
+                .max()
+                .unwrap_or(0);
+        if continuity < max_derivative_degree {
             return Err(Box::new(
                 KnotError("maximal derivatives_values degree is greater than continuity".to_string())
             ))
@@ -21,6 +22,43 @@ impl Knot {
 
         Ok(Knot { x, y, continuity, derivatives_values })
     }
+
+    pub fn c0(x: f64, y: f64) -> Self {
+        Knot { x, y, continuity: 0, derivatives_values: HashMap::new() }
+    }
+
+    pub fn c1(x: f64, y: f64) -> Self {
+        Knot { x, y, continuity: 1, derivatives_values: HashMap::new() }
+    }
+
+    pub fn c2(x: f64, y: f64) -> Self {
+        Knot { x, y, continuity: 2, derivatives_values: HashMap::new() }
+    }
+
+    pub fn cn(x: f64, y:f64, continuity: usize) -> Self {
+        Knot { x, y, continuity, derivatives_values: HashMap::new() }
+    }
+
+    pub fn fix0(x: f64, y: f64) -> Self {
+        Knot { x, y, continuity: 0, derivatives_values: HashMap::new() }
+    }
+
+    pub fn fix1(x: f64, y: f64, first_derivative: f64) -> Self {
+        Knot { x, y, continuity: 1, derivatives_values: HashMap::from([(1, first_derivative)]) }
+    }
+
+    pub fn fix2(x: f64, y: f64, first_derivative: f64, second_derivative: f64) -> Self {
+        Knot { x, y, continuity: 2, derivatives_values: HashMap::from([(1, first_derivative), (2, second_derivative)]) }
+    }
+
+    pub fn fixn(x: f64, y: f64, derivatives_values: HashMap<usize, f64>) -> Self {
+        let max_derivative_degree = derivatives_values.iter()
+                .map(|kv| *kv.0)
+                .max()
+                .unwrap_or(0);
+        Knot { x, y, continuity: max_derivative_degree, derivatives_values }
+    }
+
 }
 
 impl Ord for Knot {
@@ -53,3 +91,136 @@ impl Display for KnotError {
 }
 
 impl Error for KnotError {}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let x = 1.0;
+        let y = 2.5;
+        let continuity = 2;
+        let derivatives_values = HashMap::from([(1, 4.0)]);
+        let knot = Knot::new(x, y, continuity, derivatives_values).unwrap();
+
+        assert_eq!(x, knot.x);
+        assert_eq!(y, knot.y);
+        assert_eq!(continuity, knot.continuity);
+        assert_eq!(1, knot.derivatives_values.len());
+        assert_eq!(4.0, *knot.derivatives_values.get(&1).unwrap());
+    }
+
+    #[test]
+    fn test_c0() {
+        let x = 1.0;
+        let y = 2.5;
+        let knot = Knot::c0(x, y);
+
+        assert_eq!(x, knot.x);
+        assert_eq!(y, knot.y);
+        assert_eq!(0, knot.continuity);
+        assert_eq!(0, knot.derivatives_values.len());
+    }
+
+    #[test]
+    fn test_c1() {
+        let x = 1.0;
+        let y = 2.5;
+        let knot = Knot::c1(x, y);
+
+        assert_eq!(x, knot.x);
+        assert_eq!(y, knot.y);
+        assert_eq!(1, knot.continuity);
+        assert_eq!(0, knot.derivatives_values.len());
+    }
+
+    #[test]
+    fn test_c2() {
+        let x = 1.0;
+        let y = 2.5;
+        let knot = Knot::c2(x, y);
+
+        assert_eq!(x, knot.x);
+        assert_eq!(y, knot.y);
+        assert_eq!(2, knot.continuity);
+        assert_eq!(0, knot.derivatives_values.len());
+    }
+
+    #[test]
+    fn test_cn() {
+        let x = 1.0;
+        let y = 2.5;
+        let continuity = 3;
+        let knot = Knot::cn(x, y, continuity);
+
+        assert_eq!(x, knot.x);
+        assert_eq!(y, knot.y);
+        assert_eq!(continuity, knot.continuity);
+        assert_eq!(0, knot.derivatives_values.len());
+    }
+
+    #[test]
+    fn test_fix0() {
+        let x = 1.0;
+        let y = 2.5;
+        let knot = Knot::fix0(x, y);
+
+        assert_eq!(x, knot.x);
+        assert_eq!(y, knot.y);
+        assert_eq!(0, knot.continuity);
+        assert_eq!(0, knot.derivatives_values.len());
+    }
+
+    #[test]
+    fn test_fix1() {
+        let x = 1.0;
+        let y = 2.5;
+        let first_derivative = 0.5;
+        let knot = Knot::fix1(x, y, first_derivative);
+
+        assert_eq!(x, knot.x);
+        assert_eq!(y, knot.y);
+        assert_eq!(1, knot.continuity);
+        assert_eq!(1, knot.derivatives_values.len());
+        assert_eq!(first_derivative, *knot.derivatives_values.get(&1).unwrap());
+    }
+
+    #[test]
+    fn test_fix2() {
+        let x = 1.0;
+        let y = 2.5;
+        let first_derivative = 0.5;
+        let second_derivative = -3.5;
+        let knot = Knot::fix2(x, y, first_derivative, second_derivative);
+
+        assert_eq!(x, knot.x);
+        assert_eq!(y, knot.y);
+        assert_eq!(2, knot.continuity);
+        assert_eq!(2, knot.derivatives_values.len());
+        assert_eq!(first_derivative, *knot.derivatives_values.get(&1).unwrap());
+        assert_eq!(second_derivative, *knot.derivatives_values.get(&2).unwrap());
+    }
+
+    #[test]
+    fn test_fixn() {
+        let x = 1.0;
+        let y = 2.5;
+        let first_derivative = 0.5;
+        let second_derivative = -3.5;
+        let third_derivative = 5.0;
+
+        let derivatives_values = HashMap::from([(1, first_derivative), (2, second_derivative), (3, third_derivative)]);
+
+        let knot = Knot::fixn(x, y, derivatives_values);
+
+        assert_eq!(x, knot.x);
+        assert_eq!(y, knot.y);
+        assert_eq!(3, knot.continuity);
+        assert_eq!(3, knot.derivatives_values.len());
+        assert_eq!(first_derivative, *knot.derivatives_values.get(&1).unwrap());
+        assert_eq!(second_derivative, *knot.derivatives_values.get(&2).unwrap());
+        assert_eq!(third_derivative, *knot.derivatives_values.get(&3).unwrap());
+    }
+}
